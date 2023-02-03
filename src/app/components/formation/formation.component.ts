e0583f5dee62c28171912c2e390cfed0e6e424db
DevAHP <andrehp.profissional@gmail.com>
2023-02-03 00:29:51 -0300

    update

--- src/app/components/formation/formation.component.ts
+++ src/app/components/formation/formation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-formation',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formation.component.css']
 })
 export class FormationComponent implements OnInit {
+
+  @Input()
+  formationImg: string = '';
 
   constructor() { }
 
